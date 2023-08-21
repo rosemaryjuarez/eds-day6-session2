5a8164a10d074c479b4b903713aebe2b57a44aa6
rosemaryjuarez <rosemaryjuarez2001@gmail.com>
2023-08-21 14:23:12 -0700

    last of filter

--- dypler-wrangling.R
+++ dypler-wrangling.R
@@ -30,4 +30,32 @@ ggplot(data = pie_crab, aes(x = water_temp, y = size)) + geom_point()
 
 
 pie_sites <- pie_crab %>% filter(site %in% c('NIB', 'ZI', 'DB', 'JC'))
-d
+
+
+sites <- c('CC', 'BB', 'PIE')
+
+pie_sites_2 <- pie_crab %>% filter(site %in% sites)
+## DONT USE DOUBLW ==, it will look for vectors but IN ORDER
+## %in% is a non-order specific thing
+
+#create a subset using the %in% operator that includes sites PIE, ZI, NIB, BB, and CC
+sites2 <- c('PIE', 'ZI', 'NIB', 'BB', 'CC')
+
+pie_crab %>% filter(site %in% sites2)
+
+
+# exclude filter statements
+#
+#  != (asks is this Not equal to that value?)
+
+exclude_zi <- pie_crab %>% filter(site!= 'ZI')
+
+# what if i want to exclude sites 'BB', 'CC', 'PIE'
+
+exclude_bb_cc_pie <- pie_crab %>% filter(!site %in% c('BB', 'CC', 'PIE'))
+
+#create a subset from pie_crab that only contains observations from NIB, CC, and ZI, for crabs with carapace size exceeeding 13
+
+
+crabs_large <- pie_crab %>% filter(site %in% c('NIB','CC', 'ZI'), size > 13)
+
